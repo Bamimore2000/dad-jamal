d4aa422bd8872547905011f2f30757819a7feb2b
Bamimore Sogo <emmanuelar35@gmail.com>
2025-11-07 05:08:04 +0100

    ad

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,11 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "./generated/prisma/client"; // correct if client folder exists
 
 declare global {
-  // allow global `prisma` in dev so it doesn't recreate on hot reload
   var prisma: PrismaClient | undefined;
 }
 
 const prisma = global.prisma || new PrismaClient();
 
-// in development, attach to global to avoid multiple instances
 if (process.env.NODE_ENV === "development") {
   global.prisma = prisma;
 }
